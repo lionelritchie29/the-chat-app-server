d426abc729dec4596bad63d9943a847fe0773ab5
Lionel Ritchie <lionelrtchieee@gmail.com>
2023-10-28 16:55:43 +0700

    feat: enable ws connection

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const { createServer } = require("node:http");
+const { Server } = require("socket.io");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+
 const server = createServer(app);
+const io = new Server(server, { cors: { origin: "http://localhost:5173" } });
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("The Chat App server is running");
+io.on("connection", (socket) => {
+  console.log("New connection: ", socket.id);
 });
 
 server.listen(port, () => {
